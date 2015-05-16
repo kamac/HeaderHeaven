aa33f765c1eea74fd999d603022f01d45c4fa9c4
Maciej Kozik <kamac2495@gmail.com>
2015-05-16 15:35:50 +0200

    [code] humble tries towards making the code cleaner

--- code/HeaderHeaven/include/Parser.h
+++ code/HeaderHeaven/include/Parser.h
@@ -3,36 +3,41 @@
 * modify it under the terms of the GNU General Public License
 * as published by the Free Software Foundation; either version 2
 * of the License, or (at your option) any later version.
-* 
+*
 * This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
-* 
+*
 * You should have received a copy of the GNU General Public License
 * along with this program; if not, write to the Free Software
 * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#ifndef HHEAVEN_GENERATOR_H
-#define HHEAVEN_GENERATOR_H
+#ifndef HHEAVEN_PARSER_H
+#define HHEAVEN_PARSER_H
 
 #include "Common.h"
 #include "Lexer.h"
 #include "LanguageComponents.h"
 
-class Generator
+class Parser
 {
 public:
-	Generator();
-	~Generator();
+	Parser(const std::string& aSource);
+	~Parser();
 
-	void Generate(const std::string& aInPath, const std::string& aOutPath);
+	void ParseHeader(std::vector<Class>& aOutClasses);
+	void ParseSource(std::vector<Function>& aOutFunctions);
+	bool TryParseNamespace(std::vector<Class> &aOutClasses);
+	bool TryParseClass(Class &aOutClass);
+	bool TryParseType(std::string& aOutType);
+	bool TryParseFunction(Function &aOutFunc, const std::string &aClassName);
+	bool TryParseVarDeclaration(Variable& aOutVar);
+	bool IsTypeName(const std::string& aIdentifier) const;
 
 private:
-	void GenerateCPP(const std::string& aHeaderSource, std::string& aSource);
-	void GenerateClass(const Class& aClass, const std::string& aNameSpace, std::string& aCPPsrc, std::vector<Function>& aDeclaredFuncs) const;
-	std::string GenerateMethod(const Function& aMethod, const std::string& aNameSpace) const;
+	Lexer m_lexer;
 };
 
 #endif
---
+++ code/HeaderHeaven/src/Parser.cpp
@@ -0,0 +1,388 @@
+#include "Parser.h"
+#include <algorithm>
+
+Parser::Parser(const std::string& aSource)
+{
+	m_lexer.SetCode(aSource);
+}
+
+Parser::~Parser()
+{
+
+}
+
+void Parser::ParseHeader(std::vector<Class>& aOutClasses)
+{
+	do {
+		TryParseNamespace(aOutClasses);
+		Class cClass;
+		if (TryParseClass(cClass)) {
+			aOutClasses.push_back(cClass);
+		}
+	} while (m_lexer.PeekToken().id != Lexer::TOK_EOF);
+}
+
+void Parser::ParseSource(std::vector<Function>& aOutFunctions)
+{
+	do {
+		Function func;
+		if (TryParseFunction(func, ""))
+			aOutFunctions.push_back(func);
+	} while (m_lexer.PeekToken().id != Lexer::TOK_EOF);
+}
+
+bool Parser::TryParseNamespace(std::vector<Class> &aOutClasses)
+{
+	Lexer::Token peekToken = m_lexer.PeekToken();
+	if (!(peekToken.id == Lexer::TOK_IDENTIFIER && peekToken.strId == "namespace"))
+		return false;
+	m_lexer.NextToken();
+
+	if (m_lexer.NextToken().id != Lexer::TOK_IDENTIFIER)
+		return false; // syntax error
+
+	std::string nameSpace = m_lexer.currTok.strId;
+	if (m_lexer.NextToken().id != '{')
+		return false; // syntax error
+
+	do {
+		std::vector<Class> newClasses;
+		if (TryParseNamespace(newClasses)) {
+			for (uint16_t i = 0; i < newClasses.size(); i++) {
+				if (newClasses[i].nameSpace.empty())
+					newClasses[i].nameSpace = nameSpace;
+				else
+					newClasses[i].nameSpace = nameSpace + "::" + newClasses[i].nameSpace;
+				aOutClasses.push_back(newClasses[i]);
+			}
+		}
+
+		Class cClass;
+		if (TryParseClass(cClass)) {
+			cClass.nameSpace = nameSpace;
+			aOutClasses.push_back(cClass);
+		}
+	} while (m_lexer.PeekToken().id != '}');
+	m_lexer.NextToken();
+}
+
+bool Parser::TryParseClass(Class &aOutClass)
+{
+	Lexer::Token peekToken = m_lexer.PeekToken();
+	if (!(peekToken.id == Lexer::TOK_IDENTIFIER && (peekToken.strId == "class" || peekToken.strId == "struct")))
+		return false;
+	m_lexer.NextToken();
+
+	// this may be a class/struct declaration
+	// process all rubbish up to either ; or {. Take the last identifier as the name of the class.
+	aOutClass.name = "";
+	while (m_lexer.NextToken().id != Lexer::TOK_EOF) {
+		if (m_lexer.currTok.id == ';') {
+			// false alarm. Just a forward class/struct declaration. Disregard.
+			return false;
+		}
+		else if (m_lexer.currTok.id == '{') {
+			break;
+		}
+		else {
+			Lexer::Token peekToken = m_lexer.PeekToken();
+			if ((peekToken.id == ':' || peekToken.id == '{') && aOutClass.name.empty())
+				aOutClass.name = m_lexer.currTok.strId;
+		}
+	}
+	// process class' body
+	bool parsedFunctionBody = false;
+	while (m_lexer.currTok.id != Lexer::TOK_EOF) {
+		parsedFunctionBody = false;
+		Lexer::Token peekToken = m_lexer.PeekToken();
+		if (peekToken.id == Lexer::TOK_IDENTIFIER)
+		{
+			if (peekToken.strId == "class" || peekToken.strId == "struct") {
+				Class subClass;
+				if (TryParseClass(subClass))
+				{
+					aOutClass.classes.push_back(subClass);
+				}
+			}
+			else if (peekToken.strId == "public" || peekToken.strId == "protected" || peekToken.strId == "private")
+			{
+				m_lexer.NextToken();
+				if (m_lexer.NextToken().id != ':')
+					return false; // syntax error
+			}
+		}
+		Function func;
+		if (TryParseFunction(func, aOutClass.name)) {
+			aOutClass.methods.push_back(func);
+			if (m_lexer.currTok.id == '}')
+				parsedFunctionBody = true;
+		}
+		if (m_lexer.currTok.id == '}' && !parsedFunctionBody)
+			break;
+	}
+	if (m_lexer.NextToken().id != ';')
+		return false; // syntax error
+	return true;
+}
+
+bool Parser::TryParseFunction(Function &aOutFunc, const std::string &aClassName)
+{
+	auto SplitFuncName = [](const std::string& aInput) {
+		std::pair<std::string, std::string> ret;
+		auto separator = aInput.rfind("::");
+		if (separator != std::string::npos) {
+			ret.first = aInput.substr(0, separator);
+			ret.second = aInput.substr(separator + 2, aInput.size());
+		}
+		else {
+			ret.second = aInput;
+		}
+		return ret;
+	};
+	bool isFunctionDeclaration = false;
+	Lexer::Token peekToken = m_lexer.PeekToken();
+	auto funcName = SplitFuncName(peekToken.strId);
+	// constructor
+	if (peekToken.id == Lexer::TOK_IDENTIFIER) {
+		if ((funcName.second == aClassName || funcName.second == SplitFuncName(funcName.first).second) && m_lexer.PeekToken(2).id == '(') {
+			m_lexer.NextToken(); m_lexer.NextToken(); // skip to '('
+			isFunctionDeclaration = true;
+			aOutFunc.declaration.name = funcName.second;
+			aOutFunc.declaration.returnType = "";
+			aOutFunc.nameSpace = funcName.first;
+		}
+	}
+	// destructor
+	if (funcName.second.size() > 0 && funcName.second[0] == '~') {
+		m_lexer.NextToken();
+		if ((funcName.second == ('~' + SplitFuncName(funcName.first).second) || funcName.second == '~' + aClassName) && m_lexer.PeekToken().id == '(') {
+			m_lexer.NextToken(); // skip to '('
+			isFunctionDeclaration = true;
+			aOutFunc.declaration.name = funcName.second;
+			aOutFunc.declaration.returnType = "";
+			aOutFunc.nameSpace = funcName.first;
+		}
+	}
+	std::string type = "";
+	if (!isFunctionDeclaration && TryParseType(type)) {
+		std::string name = "";
+		// this could be a method initialization (that is not a constructor/destructor).
+		isFunctionDeclaration = true;
+		// process all rubbish up to either ; or (. Take the last identifier as the name.
+		while (m_lexer.currTok.id != Lexer::TOK_EOF) {
+			if (m_lexer.currTok.id == ';') {
+				// false alarm. Just a variable declaration. Disregard.
+				isFunctionDeclaration = false;
+				break;
+			}
+			else if (m_lexer.currTok.id == '(') {
+				break;
+			}
+			else {
+				name = m_lexer.currTok.strId;
+			}
+			m_lexer.NextToken();
+		}
+		if (!isFunctionDeclaration) return false;
+		auto funcName = SplitFuncName(name);
+		aOutFunc.declaration.name = funcName.second;
+		aOutFunc.declaration.returnType = type;
+		aOutFunc.nameSpace = funcName.first;
+	}
+	if (isFunctionDeclaration) {
+		// parse arguments if present
+		do {
+			Variable var;
+			if (!TryParseVarDeclaration(var)) {
+				if (m_lexer.currTok.id == ')')
+					break;
+				return false;
+			}
+			aOutFunc.arguments.push_back(var);
+			Lexer::Token peekToken = m_lexer.PeekToken();
+			if (peekToken.id == ',') {
+				m_lexer.NextToken();
+			}
+			else if (peekToken.id == ')') {
+				m_lexer.NextToken();
+				break;
+			}
+		} while (true);
+
+		m_lexer.NextToken();
+		if (m_lexer.currTok.id == Lexer::TOK_IDENTIFIER && m_lexer.currTok.strId == "const") {
+			aOutFunc.isConst = true;
+			m_lexer.NextToken();
+		}
+		bool isInterfaceMethod = false;
+		if (m_lexer.currTok.id == '=') {
+			// interface method, skip the = 0
+			m_lexer.NextToken();
+			if (m_lexer.currTok.id != Lexer::TOK_INTEGER)
+				return false;
+			m_lexer.NextToken();
+			isInterfaceMethod = true;
+			aOutFunc.hasBody = true;
+		}
+		if (m_lexer.currTok.id == '{') {
+			aOutFunc.hasBody = true;
+			// skip the body
+			uint16_t nested = 0;
+			do {
+				m_lexer.NextToken();
+				if (m_lexer.currTok.id == '{')
+					nested++;
+				else if (m_lexer.currTok.id == '}') {
+					if (nested > 0)
+						nested--;
+				}
+			} while (m_lexer.currTok.id != '}' && nested == 0);
+			aOutFunc.endIndex = m_lexer.GetCursor();
+		}
+		else if (m_lexer.currTok.id == ';') {
+			if (!isInterfaceMethod)
+				aOutFunc.hasBody = false;
+		}
+		return true;
+	}
+	return false;
+}
+
+bool Parser::TryParseType(std::string& aOutType)
+{
+	if (m_lexer.NextToken().id != Lexer::TOK_IDENTIFIER)
+		return false;
+	aOutType = m_lexer.currTok.strId;
+	if (!IsTypeName(aOutType))
+		return false;
+	if (aOutType == "const" || aOutType == "unsigned" || aOutType == "signed") {
+		std::string properType = "";
+		if (!TryParseType(properType))
+			return false;
+		aOutType += " " + properType;
+		return true;
+	}
+	if (m_lexer.PeekToken().id == '<')
+	{
+		m_lexer.NextToken();
+		// we have a complicated type to deal with. (like std::map<std::string, std::vector<T>>)
+		aOutType += '<';
+		do {
+			std::string innerType = "";
+			bool gotInnerType = TryParseType(innerType);
+			// a syntax error occured
+			if (!gotInnerType)
+				return false;
+			aOutType += innerType;
+			m_lexer.NextToken();
+			if (m_lexer.currTok.id == ',' || m_lexer.currTok.id == '>')
+				aOutType += m_lexer.currTok.id;
+			else
+				return false; // a syntax error ocurred
+		} while (m_lexer.currTok.id == ',' && m_lexer.currTok.id != '>' && m_lexer.currTok.id != Lexer::TOK_EOF);
+		if (m_lexer.currTok.id == Lexer::TOK_EOF)
+			return false; // syntax error
+		if (m_lexer.PeekToken().id == ':')
+		{
+			m_lexer.NextToken();
+			if (m_lexer.PeekToken().id == ':') {
+				m_lexer.NextToken();
+				std::string additionalType = "";
+				if (TryParseType(additionalType))
+					aOutType += "::" + additionalType;
+				else
+					return false;
+			}
+			else {
+				return false; // syntax error
+			}
+		}
+	}
+	Lexer::Token peekToken = m_lexer.PeekToken();
+	while (peekToken.id == '&' || peekToken.id == '*') {
+		m_lexer.NextToken();
+		aOutType += m_lexer.currTok.id;
+		peekToken = m_lexer.PeekToken();
+	}
+	return true;
+}
+
+bool Parser::TryParseVarDeclaration(Variable& aOutVar)
+{
+	if (!TryParseType(aOutVar.returnType))
+		return false;
+	if (m_lexer.NextToken().id != Lexer::TOK_IDENTIFIER)
+		return false;
+	aOutVar.name = m_lexer.currTok.strId;
+	if (m_lexer.PeekToken().id == '=') {
+		m_lexer.NextToken();
+		m_lexer.NextToken();
+	}
+	return true;
+}
+
+bool Parser::IsTypeName(const std::string& aIdentifier) const
+{
+	// we try to guess if it's a type name by matching it against reserved keywords that
+	// are not types.
+	if (
+		aIdentifier == "auto" ||
+		aIdentifier == "break" ||
+		aIdentifier == "case" ||
+		aIdentifier == "catch" ||
+		aIdentifier == "class" ||
+		aIdentifier == "constexpr" ||
+		aIdentifier == "const_cast" ||
+		aIdentifier == "continue" ||
+		aIdentifier == "decltype" ||
+		aIdentifier == "default" ||
+		aIdentifier == "delete" ||
+		aIdentifier == "do" ||
+		aIdentifier == "dynamic_cast" ||
+		aIdentifier == "else" ||
+		aIdentifier == "enum" ||
+		aIdentifier == "explicit" ||
+		aIdentifier == "export" ||
+		aIdentifier == "extern" ||
+		aIdentifier == "false" ||
+		aIdentifier == "for" ||
+		aIdentifier == "friend" ||
+		aIdentifier == "goto" ||
+		aIdentifier == "if" ||
+		aIdentifier == "inline" ||
+		aIdentifier == "mutable" ||
+		aIdentifier == "namespace" ||
+		aIdentifier == "new" ||
+		aIdentifier == "nullptr" ||
+		aIdentifier == "operator" ||
+		aIdentifier == "private" ||
+		aIdentifier == "protected" ||
+		aIdentifier == "public" ||
+		aIdentifier == "register" ||
+		aIdentifier == "reinterpret_cast" ||
+		aIdentifier == "return" ||
+		aIdentifier == "sizeof" ||
+		aIdentifier == "static" ||
+		aIdentifier == "static_assert" ||
+		aIdentifier == "static_cast" ||
+		aIdentifier == "struct" ||
+		aIdentifier == "switch" ||
+		aIdentifier == "template" ||
+		aIdentifier == "this" ||
+		aIdentifier == "throw" ||
+		aIdentifier == "true" ||
+		aIdentifier == "try" ||
+		aIdentifier == "typedef" ||
+		aIdentifier == "typeid" ||
+		aIdentifier == "typename" ||
+		aIdentifier == "union" ||
+		aIdentifier == "using" ||
+		aIdentifier == "virtual" ||
+		aIdentifier == "volatile" ||
+		aIdentifier == "while") {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
